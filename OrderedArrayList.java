3c750a032fdf014d159b2b825b8d8a55b9796286
jyagupsky20 <jyagupsky20@stuy.edu>
2021-12-12 15:35:35 -0500

    OrderedArrayList

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,3 +1,38 @@
+import java.util.ArrayList;
 public class OrderedArrayList{
-  
+  private ArrayList<Integer> _data;
+  //Default constructor
+  public OrderedArrayList(){
+    _data = new ArrayList<Integer>();
+  }
+  //toString method
+  public String toString(){
+    return _data.toString();
+  }
+  //Add method: adds just before the first element that is larger
+  public boolean add(int element){
+    for(int i = 0; i < size(); i++){
+      if(_data.get(i) > element){
+        _data.add(i,element);
+        return true;
+      }
+    }
+    _data.add(element);
+    return true;
+  }
+  //Get method
+  public int get(int index){
+    return _data.get(index);
+  }
+  public int size(){
+    return _data.size();
+  }
+  public static void main(String[] args) {
+    OrderedArrayList bob = new OrderedArrayList();
+    System.out.println(bob);
+    for (int i = 0; i < 23; i++){
+      bob.add((int)(Math.random()*10));
+    }
+    System.out.println(bob);
+  }
 }
